0231d2defb9d8ea1b7d65e84d5ad2b016c17d43e
DRAKULAAAA <149157312+DRAKULAAAA@users.noreply.github.com>
2023-10-27 13:31:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,274 @@
+/* console.log("funkcja _ deklaracja, parametry, return")
+
+function przedstawMnie() {
+    console.log("Mam na imie Jacob")
+}
+przedstawMnie();
+
+const wypiszMojeImie = function () {
+    console.log("Moje imie to Jacob")
+}
+wypiszMojeImie();
+
+const dodajLiczby = function (liczba1 = 0, liczba2 = 0) {
+    let wynik = liczba1 + liczba2;
+    console.log(wynik);
+}
+let liczbaHansa = 100;
+let liczbaPierwsza = 1;
+let LiczbaDruga = 10;
+dodajLiczby(liczbaHansa, 8);
+dodajLiczby(10, 20);
+
+const mnożenieLiczb = function (liczba1 = 0, liczba2 = 0) {
+    let wynik = liczba1 * liczba2;
+    console.log(wynik)
+}
+mnożenieLiczb(2, 5); 
+
+console.log("Funkcja strzałkowa")
+
+const dokwadratu = x => x * x;
+
+let wynik = dokwadratu(3);
+
+console.log(wynik); 
+
+console.log("Zasięg zmiennych");
+
+var imie = "Jacob";
+
+console.log(imie);
+
+function wyswietlImie() {
+    var imie = "Monika";
+    var nazwisko = "Kociołek";
+    console.log(imie)
+    console.log(nazwisko);
+}
+
+wyswietlImie();
+
+    if (imie == 'Jacob') {
+        var wiek = 16;
+
+        console.log(wiek);
+    }
+
+console.log(wiek);
+
+console.log("Typy danych - Typy proste");
+
+let numerPierwszy = 100;
+let numerDrugi = 100;
+
+let mojeImie = "Jacob";
+let mojeDrugieImie = "Tomson";
+
+let jestprogramista = true;
+
+let obiekt = null;
+let mainHeading = document.getElementById("mainHeading");
+
+let typuUndefined;
+
+if(jestprogramista){
+    console.log(mojeImie + " jest programistą");
+}
+
+let symbol = Symbol();
+
+const osoba = {
+    name: "Jacob",
+    occupation: "Programista",
+    [symbol]: "Tajne hasło"
+}
+symbol = Symbol();
+
+osoba[symbol] = "tajne dane"
+
+
+console.log(osoba[symbol]); */
+
+// console.log("funkcja _ deklaracja, parametry, return")
+
+// function przedstawMnie() {
+//     console.log("Mam na imie Jacob")
+// }
+// przedstawMnie();
+
+// const wypiszMojeImie = function () {
+//     console.log("Moje imie to Jacob")
+// }
+// wypiszMojeImie();
+
+// const dodajLiczby = function (liczba1 = 0, liczba2 = 0) {
+//     let wynik = liczba1 + liczba2;
+//     console.log(wynik);
+// }
+// let liczbaHansa = 100;
+// let liczbaPierwsza = 1;
+// let LiczbaDruga = 10;
+// dodajLiczby(liczbaHansa, 8);
+// dodajLiczby(10, 20);
+
+// const mnożenieLiczb = function (liczba1 = 0, liczba2 = 0) {
+//     let wynik = liczba1 * liczba2;
+//     console.log(wynik)
+// }
+// mnożenieLiczb(2, 5); 
+
+// console.log("Funkcja strzałkowa")
+
+// const dokwadratu = x => x * x;
+
+// let wynik = dokwadratu(3);
+
+// console.log(wynik); 
+
+// console.log("Zasięg zmiennych");
+
+// var imie = "Jacob";
+
+// console.log(imie);
+
+// function wyswietlImie() {
+//     var imie = "Monika";
+//     var nazwisko = "Kociołek";
+//     console.log(imie)
+//     console.log(nazwisko);
+// }
+
+// wyswietlImie();
+
+//     if (imie == 'Jacob') {
+//         var wiek = 16;
+
+//         console.log(wiek);
+//     }
+
+// console.log(wiek);
+
+// console.log("Typy danych - Typy proste");
+
+// let numerPierwszy = 100;
+// let numerDrugi = 100;
+
+// let mojeImie = "Jacob";
+// let mojeDrugieImie = "Tomson";
+
+// let jestprogramista = true;
+
+// let obiekt = null;
+// let mainHeading = document.getElementById("mainHeading");
+
+// let typuUndefined;
+
+// if(jestprogramista){
+//     console.log(mojeImie + " jest programistą");
+// }
+
+// let symbol = Symbol();
+// const osoba = {
+//     name: "Jacob",
+//     occupation: "Programista",
+//     [symbol]: "Tajne hasło"
+// }
+// symbol = Symbol();
+
+// osoba[symbol] = "tajne dane"
+
+
+// console.log(osoba[symbol]);
+
+// const symbols = Object.getOwnPropertySymbols(osoba);
+
+// for( let sym of symbols ) {
+//     console.log(osoba[sym]);
+// } 
+// console.log("Typy danych - Typy referencyjne");
+
+// let number = 100;
+// let secondNumber = number;
+
+// secondNumber = 200
+
+// console.log(secondNumber);
+
+// // tablice
+
+// let tablica = [4, 2, 0];
+// let drugaTablica = tablica;
+
+// drugaTablica[2] = 111;
+
+// let osoba = {
+//     name: "Jacob",
+//     age: 25,
+// };
+// let drugaOsoba = osoba;
+
+// drugaOsoba.age = 20;
+
+// console.log(typeof number);
+// console.log(typeof secondNumber);
+
+// console.log(typeof osoba);
+// console.log(typeof drugaTablica)
+
+/* console.log("Tablice - indeksowanie tablic")
+
+let names = ["Hans", "Jacob", "Adam"];
+
+names[1] = 'Andrzej';
+names[2] = 'Bartek';
+
+let [imieProgramisty, imieSekretarza, imieGrafika] = names;
+
+console.log(imieProgramisty);
+console.log(imieSekretarza);
+console.log(imieGrafika);
+ */
+
+
+/* console.log("object math");
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.LN2);
+console.log(Math.LN10);
+
+console.log(Math.sin(0));
+console.log(Math.sin(Math.PI/2));
+console.log(Math.cos(0));
+console.log(Math.cos(Math.PI));
+
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(6));
+
+console.log(Math.random());
+console.log(Math.floor(4.99));
+
+console.log(Math.floor(Math.random() *101 ));
+console.log(Math.floor(Math.random() *10 ) +1);
+
+console.log(Math.round(108.50)); */
+
+
+/* console.log("huj");
+
+let realut = 3 + 6;
+realut = 3 - 2;
+realut = 2 * 5;
+realut = 1 / 2;
+realut = 7 % 5;
+
+let i = 100;
+
+
+console.log(i++);
+console.log(i);
+/* console.log(realut) */ 
+
+console.log("huj2");
+
